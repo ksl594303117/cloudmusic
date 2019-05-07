267b887091c58f982e33ede1f4ddd2c9c1739f3d
 寇姝玲 <koushuling@soyoung.com>
2019-05-07 16:43:21 +0800

    music

---
+++ precache-manifest.618046a1eda543a9ae9ae6f1f58f3016.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dc6fdc67611efce36a3c500cbe1c0ec",
+    "url": "/cloudmusic/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "df10d3e311e52592792f",
+    "url": "/cloudmusic/static/js/vendor.df10d3e3.js"
+  },
+  {
+    "revision": "fdcf63345da4d6510fbbbfaac321806d",
+    "url": "/cloudmusic/static/manifest.json"
+  },
+  {
+    "revision": "05c4050b832951802ad9",
+    "url": "/cloudmusic/static/js/vue.05c4050b.js"
+  },
+  {
+    "revision": "3742528143340e36915adf388d64060f",
+    "url": "/cloudmusic/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "16e66f20314b771221e9",
+    "url": "/cloudmusic/static/js/manifest.16e66f20.js"
+  },
+  {
+    "revision": "588079df8f1b8fde3b3cd691ee65bf06",
+    "url": "/cloudmusic/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "83409fb3fe88cb94d7d3df5fe97569c9",
+    "url": "/cloudmusic/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/cloudmusic/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "20cf34e790c1b676a90d",
+    "url": "/cloudmusic/static/js/index.20cf34e7.js"
+  },
+  {
+    "revision": "0ff69c8fca81469c28f589d5fad0616e",
+    "url": "/cloudmusic/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "047f3021e153c19951f86dd209904243",
+    "url": "/cloudmusic/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b451e38c6ee4b03781b1019c518e7894",
+    "url": "/cloudmusic/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "efbe0aa485b3aeaceb8ead9191f0c4c0",
+    "url": "/cloudmusic/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b8a42c4a3b00c4f75bc636f2923eaeca",
+    "url": "/cloudmusic/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "20cf34e790c1b676a90d",
+    "url": "/cloudmusic/static/css/index.1733ae11.css"
+  },
+  {
+    "revision": "390bf04223f6255249443801b4b7b1eb",
+    "url": "/cloudmusic/index.html"
+  },
+  {
+    "revision": "7264d2ec7553bb96307af4acbe697faf",
+    "url": "/cloudmusic/favicon.ico"
+  }
+];
